08439ba6daa1fb44abed7be8db21154629a9600a
adali <adali@2288d9a7-0fa1-3145-9870-04f04981ec47>
2012-01-04 06:35:08 +0000

    Former-commit-id: 7f37283b035ca5d2f68ae56504438db6578dbcf5

---
+++ graph_embedding_fn.R
@@ -0,0 +1,312 @@
+
+genG <- function(s=seed,n=1000,np=50,p=0.1,q=0.5) {
+	x <- matrix(p, nrow=n, ncol=n)    ## null: (100-np)x98 matrix
+	if (np>0) {
+		egg <- matrix(q, nrow=np, ncol=np) ## alternative: npx98 matrix
+		x[(n-np+1):n,(n-np+1):n] <- egg
+	}
+	ordering<-sample.int(n)
+	diag(x) <- 0
+	
+	A <- matrix(0,nrow=n,ncol=n)
+	for(i in 1:(n-1))
+		for(j in i:n)
+			A[i,j] <- A[j,i] <- sample(c(0,1),1,prob=c(1-x[i,j],x[i,j]))
+	
+	return(A)
+}
+
+
+perturbG<-function(G,q){
+	n<-nrow(G)
+	Flip.mat<-matrix(0,nrow=n,ncol=n)
+	for(i in 1:(n-1))
+		for(j in i:n){
+			Flip.mat[i,j]<-sample(c(0,1),1,prob=c(1-q,q))
+			
+		}
+	for(i in 1:(n-1))
+		for(j in i:n){
+			if (Flip.mat[i,j]==1){
+				G[i,j]<-1-G[i,j]
+				G[j,i]<-1-G[j,i]
+			}
+			
+		}
+	return(G)
+	
+}
+
+
+
+jofc<-function(G,Gp,
+		in.sample.ind,
+		d.dim,
+		graph.is.directed=FALSE,
+		notconnect.wt=10,
+		use.weighted.graph=TRUE,
+		wt.matrix.1=NULL,
+		wt.matrix.2=NULL,
+		sep.graphs=TRUE # if TRUE, treat two graphs separately to compute dissimilarities
+#and impute W (off-diagonalblock matrix)
+# if FALSE, join the graphs and compute dissimilarities from joint graph
+){
+	
+	n<-nrow(G)
+	graph.mode<- ifelse(graph.is.directed,"directed","undirected")
+	
+	Graph.1<-graph.empty(directed=graph.is.directed)
+	Graph.2<-graph.empty(directed=graph.is.directed)
+	Graph.M<-graph.empty(directed=graph.is.directed)
+	
+	if (!use.weighted.graph) {
+		#Given adjacency matrix, generate unweighted graph
+		print("Using adjacency for computing dissimilarities")
+		Graph.1<-graph.adjacency(G, mode=graph.mode)
+		Graph.2<-graph.adjacency(Gp,mode=graph.mode)
+		A.M<- diag(n)
+		G.comb<-omnibusM(G,Gp,A.M)
+		Graph.M <- graph.adjacency(G.comb,
+				weighted= NULL ,mode=graph.mode)
+	} else{
+		print("Using weighted graph for computing dissimilarities")
+		# If creating  a weighted graph 
+		# make the weight matrix from adjacency matrix. Those with same-condition edges have weights of wt.connect/10
+		#those  with no edges have weights of wt.connect. Those with "matched edges has weights of matched.cost
+		A.M<- matrix(500,n,n)
+		diag(A.M) <- matched.cost
+		if (is.null(wt.matrix.1)){
+			#Given adjacency matrix, generate weighted graph
+			G.w <- G
+			Gp.w <- Gp
+			G.w[G==0] <- notconnect.wt
+			
+			Gp.w[Gp==0] <- notconnect.wt
+			G.w[G==1] <- notconnect.wt/10
+			
+			Gp.w[Gp==1] <- notconnect.wt/10
+			G.comb.w<-omnibusM(G.w,Gp.w,A.M)
+			
+			if (sep.graphs){
+			Graph.1<-graph.adjacency(G.w, weighted= TRUE , mode=graph.mode)
+			Graph.2<-graph.adjacency(Gp.w,weighted= TRUE , mode=graph.mode)
+			}
+			else{
+				Graph.M <- graph.adjacency(G.comb.w,weighted= TRUE ,
+						mode=graph.mode)
+				
+			}
+		}
+		else{
+			if (sep.graphs){
+			#Given weight matrix, generate weighted graph                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+			Graph.1<-graph.adjacency(wt.matrix.1 ,weighted=TRUE,mode= graph.mode)
+			Graph.2<-graph.adjacency(wt.matrix.2,weighted=TRUE,mode=graph.mode)
+			
+				#Don't need to compute Graph.M
+				
+			}
+			else{
+			Graph.M<-graph.adjacency(omnibusM(wt.matrix.1,wt.matrix.2,(wt.matrix.1+wt.matrix.2)/2),
+					                   weighted=TRUE,mode=graph.mode)
+			}
+		}
+		
+	}
+	
+
+	
+	if (sep.graphs){
+		#compute dissimilarities in separate graphs, then impute dissimilarity between different condition
+		D.1<-shortest.paths(Graph.1)
+		D.2<-shortest.paths(Graph.2)
+		D.1[is.infinite(D.1)]<-1E10
+		D.2[is.infinite(D.2)]<-1E10
+		D.w<- (D.1+D.2)/2#mapply(min,D.1,D.2)
+		
+		D.M<- omnibusM(D.1,D.2,D.w)
+		
+	}
+	else{
+		#compute dissimilarities in joint graph
+		D.M<-shortest.paths(Graph.M)
+		D.M[is.infinite(D.M)]<-1E10
+	}
+	#print("max(D.M)")
+	#print(max(D.M))
+	
+	
+	params<-list(d=d.dim,
+			wt.equalize=FALSE,
+			separability.entries.w=FALSE,
+			assume.matched.for.oos = FALSE)
+
+	oos <- TRUE
+	
+	Embed.List<-Embed.Nodes(D.M,  in.sample.ind ,oos ,
+			d=d.dim,
+			wt.equalize=FALSE,
+			separability.entries.w=FALSE,
+			assume.matched.for.oos = FALSE)	
+	J<-matrix()
+	for (Y.embed in Embed.List){
+		Loc.Embed <-Y.embed
+		print("dim(Y.embed)")
+		print(dim(Y.embed))
+		test.samp.size<-nrow(Y.embed)/2
+		Dist=as.matrix(dist(Y.embed))[1:test.samp.size,(1:test.samp.size)+test.samp.size]
+		print("Dist")
+		print(dim(dist(Y.embed)))
+		
+		#list(loc=Loc.Embed,dist=Dist)
+		J<-Dist
+		
+	}
+	return(J)
+	
+}
+
+Embed.Nodes <-function(D.omnibus,in.sample.ind,oos, d=d.dim,
+		wt.equalize=FALSE,
+		separability.entries.w=FALSE,
+		assume.matched.for.oos = FALSE ){
+	
+	
+	Y.embeds<-list()
+	oos.use.imputed<- FALSE
+	w.max.index<-length(w.vals)
+	
+	#in.sample.ind<-which(in.sample.ind)
+	
+	# Embed in-sample using different weight matrices (differentw values)
+	if (oos){	
+		n<-sum(in.sample.ind)/2
+		test.samp.size<-sum(!in.sample.ind)/2
+		D.in <- D.omnibus[in.sample.ind,in.sample.ind]
+		init.conf<-cmdscale(d=D.in,k=d)
+		
+		
+		
+		
+		X.embeds<-JOFC.Insample.Embed(D.in,d,w.vals,separability.entries.w,init.conf=init.conf,
+				wt.equalize=wt.equalize)
+		for (l in 1:w.max.index){
+			if (verbose) print("OOS embedding for JOFC for w= \n")
+			if (verbose) print(w.vals[l])
+			
+			w.val.l <- w.vals[l]
+			X <- X.embeds[[l]]
+			
+			insample.indices<- which(in.sample.ind)
+			
+			#Compute Weight matrix corresponding in-sample  entries
+			# Since we are going to oos-embedding, set the weights  of in-sample embedding of stress
+			# We are using previous in-sample embeddings, anyway
+			oos.Weight.mat.1<-matrix(0,2*n,2*n)
+			
+			
+			
+			
+			#Compute Weight matrix corresponding OOS  entries
+			oos.Weight.mat.2<-w.val.to.W.mat(w.val.l,(2*test.samp.size),separability.entries.w,wt.equalize)
+			
+			# If assume.matched.for.oos is true, we assume OOS dissimilarities are matched(in reality,
+			# they are matched for the matched pairs, but unmatched for the unmatched pairs)
+			# If assume.matched.for.oos is true, we ignore the dissimilarities between matched/unmatched 
+			# pairs
+			if (!assume.matched.for.oos){
+				oos.Weight.mat.2[1:test.samp.size,test.samp.size+(1:test.samp.size)]<-0
+				oos.Weight.mat.2[test.samp.size+(1:test.samp.size),(1:test.samp.size)]<-0
+			}
+			
+			
+			# if (oos.use.imputed is true) we treat the dissimiilarities between  in-sample and out-of-sample measurements
+			# from different conditions like fidelity terms
+			# otherwise they are ignored
+			if (oos.use.imputed){
+				oos.Weight.mat.w <- matrix(1-w.val.l,2*n,2*test.samp.size)
+			} else{
+				oos.Weight.mat.w <- rbind(cbind(matrix(1-w.val.l,n,test.samp.size), matrix(0,n,test.samp.size) ),
+						cbind(matrix(0,n,test.samp.size),matrix(1-w.val.l,n,test.samp.size))
+				)
+			}
+			oos.Weight.mat<-omnibusM(oos.Weight.mat.1,oos.Weight.mat.2,oos.Weight.mat.w)
+			
+			
+			
+			omnibus.oos.D.0 <- rbind(
+					cbind(D.omnibus[in.sample.ind,in.sample.ind],D.omnibus[in.sample.ind,!in.sample.ind]),
+					cbind(D.omnibus[!in.sample.ind,in.sample.ind],D.omnibus[!in.sample.ind,!in.sample.ind])
+			)
+			
+			oos.Weight.mat[is.na(omnibus.oos.D.0)]<-0
+			omnibus.oos.D.0[is.na(omnibus.oos.D.0)]<-1
+			
+			if (verbose) print("JOFC null omnibus OOS embedding \n")
+			
+			Y.0t<-oosIM(D=omnibus.oos.D.0,
+					X=X,
+					init     = "random",
+					verbose  = FALSE,
+					itmax    = 1000,
+					eps      = 1e-8,
+					W        = oos.Weight.mat,
+					isWithin = NULL,
+					bwOos    = FALSE)
+			
+			if (verbose) print("JOFC alternative omnibus OOS embedding \n")
+			Y.embeds<-c(Y.embeds,list(Y.0t))
+		}
+		
+	}
+	
+	Y.embeds
+	
+}
+
+solveMarriage<- function(Dist){
+	matches<-pairmatch(Dist)
+	
+	
+}
+
+present<-function(matches){
+	true.pairings<-0
+	pair.names<-levels(M)
+	num.pairs<-length(pair.names)
+	for (i in 1:num.pairs){
+		paired.ind <-which(M==levels(M)[i])
+		if (abs(paired.ind[1]-paired.ind[2])==num.pairs){
+			true.pairings <- true.pairings + 1
+		}
+	}
+	print(paste(true.pairings," true matches  out of ", num.pairs ," pairings"))
+	return(true.pairings)
+}
+
+
+ER = function(n,p)
+{
+	A = matrix( rbinom(n^2,1,p) , ncol=n,byrow=T )
+	A=A*upper.tri(A)
+	A=A+t(A)
+	return(A)
+}
+
+bitflip = function(G,q10,q01,binary=T,symmetric=T,hollow=T)
+# takes graph (adjacency matrix) G and flips 1s to 0s with prob q10 and flips 0s to 1s with prob q01
+# assumes binary=T,symmetric=T,hollow=T
+{
+	n=dim(G)[1]
+	for(u in 1:(n-1))
+		for(v in (u+1):n)
+			G[u,v]=G[v,u]=ifelse(G[u,v],1-rbinom(1,1,q10),rbinom(1,1,q01))
+	return(G)
+}
+adj.Mat.2.P<-function(A){
+	a.avg<-rowMeans(A)
+	for (i in 1:nrow(A)) A[,i]<-A[,i]/a.avg[i]	
+}
+diff.dist<-function(P){
+	eig(P)
+}
